87243a8701310560d7d080350abb78b769c8a173
Ahmed Damasy <ahmeddamasy93@gmail.com>
2021-12-16 20:07:28 +0200

    navbar component

---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from '../core/Navbar/Navbar';
+
+class Dashboard extends Component {
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
---
+++ src/components/core/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <header className="header">
+      <img height="60" src="https://speedlebanon.com/wp-content/uploads/2015/07/bayt_logo_com_en-2.png" alt="Bayt" />
+    </header>
+  )
+}
+
+export default Navbar
